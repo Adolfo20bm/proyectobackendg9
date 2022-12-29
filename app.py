6fe1898444c1159d3272945745a636c6ba63a976
Adolfo20bm <abm2491@gmail.com>
2022-12-29 12:25:08 -0500

    modificamos el gitignore y el app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_migrate import Migrate
+from os import environ
+from dotenv import load_dotenv
+from flask_restful import Api
+
+from config import conexion
+from models.categorias import CategoriaModel
+from models.productos import ProductoModel
+from controllers.categoriaController import CategoriasController, CategoriaController
+from controllers.productoController import ProductosController, ProductoController
+
+load_dotenv()
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_ECHO'] = environ.get('MOSTRAR_SQL') 
+conexion.init_app(app)
+migrate = Migrate(app, conexion)
+
+# Declarar todas las rutas que vamos a utilizar mediante los controladores
+api.add_resource(CategoriasController, '/categorias')
+api.add_resource(CategoriaController, '/categoria/<int:id>')
+api.add_resource(ProductosController, '/productos')
+api.add_resource(ProductoController, '/producto/<int:categoriaId>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
