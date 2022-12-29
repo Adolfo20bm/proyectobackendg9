7878b691af86758cd124f07802465f698a9ec107
Adolfo20bm <abm2491@gmail.com>
2022-12-29 15:12:22 -0500

    se modifico compo descripcion por cod_patrimonial en el modelo producto

--- models/productos.py
+++ models/productos.py
@@ -13,7 +13,7 @@ class ProductoModel(conexion.Model):
     stock = Column(type_ = types.Integer, nullable=False)
     entregas = Column(type_ = types.Integer, nullable=False)
     fecha = Column(type_=types.DateTime, name='fecha', default=datetime.now())
-    descripcion = Column(type_ = types.String(length=12), nullable=False)
+    cod_patrimonial = Column(type_ = types.String(length=12), nullable=False)
       
     # La clase ForeignKey va en los argumentos adicionales (*args)
     categoriaId = Column(ForeignKey(column='categorias.id'), type_=types.Integer, nullable=False, name='categorias_id')
